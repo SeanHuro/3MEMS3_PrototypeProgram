9990fc8efb10998677d6e5ded77921b476bf5f15
SeanHuro <92795992+SeanHuro@users.noreply.github.com>
2023-04-17 19:54:17 +0100

    Add files via upload

---
+++ Prototype Program.ino
@@ -0,0 +1,24 @@
+const int motorA = 5;
+const int motorB = 6;
+
+void setup()
+{
+  pinMode(motorA, OUTPUT);
+  pinMode(motorB, OUTPUT);
+}
+
+void motor_up(){
+  analogWrite(motorA, 255);
+  digitalWrite(motorB, LOW);
+}
+
+void loop(){
+  analogWrite(motorA, 255);
+  digitalWrite(motorB, LOW);
+  delay(4000);
+  digitalWrite(motorB, HIGH);
+  delay(4000);
+}
+
+
+
